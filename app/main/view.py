4417a46c8be3553a43153c4f9e749e2522efce9e
ohjiyeon3275 <ojy940716@gmail.com>
2022-03-06 23:22:03 +0900

    templates folder for render_template path

--- app/main/view.py
+++ app/main/view.py
@@ -1,5 +1,3 @@
-# view function
-
 from ast import Str
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -9,4 +7,3 @@ class NameForm(FlaskForm) :
     name = StringField("type your name >> ", validators=[DataRequired()])
     SubmitField('SUBMIT')
 
-    
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 from flask import abort
 
 app = Flask(__name__)
 
 def index():
-    request.get_data()
     return "<h1>Hi!</h1>"
-
 app.add_url_rule('/', 'index', index) # == @app.route("/index")
 
 
@@ -16,12 +14,7 @@ def user(name):
         abort(404)
     return "hello {}".format(name)
 
+
 @app.route("/render-user/<name>", methods=["GET"])
 def user_render(name):
-    return render_template('template/user.html', name=name)
-
-
-
-#what is this function and __things__ for
-if __name__ == '__main__':
-    app.run
+    return render_template('user.html', name=name)
