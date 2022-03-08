c6582fc68f080c71e50bcb85a81841b1bb77b132
ohjiyeon3275 <ojy940716@gmail.com>
2022-03-08 22:12:46 +0900

    errorhandler custom

--- main.py
+++ main.py
@@ -11,10 +11,18 @@ app.add_url_rule('/', 'index', index) # == @app.route("/index")
 @app.route("/user/<name>", methods=["GET"])
 def user(name):
     if name == 'usererror':
-        abort(404)
+        abort(500)
     return "hello {}".format(name)
 
 
 @app.route("/render-user/<name>", methods=["GET"])
 def user_render(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
